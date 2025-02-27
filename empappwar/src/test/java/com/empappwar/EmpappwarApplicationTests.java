8839ea422016a19da127f31b58ca18b9824a3a78
Rashid T Tahasildar <85626896+Tahasildarrashid@users.noreply.github.com>
2025-02-27 09:45:18 +0530

    Add files via upload

---
+++ empappwar/src/test/java/com/empappwar/EmpappwarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empappwar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpappwarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
